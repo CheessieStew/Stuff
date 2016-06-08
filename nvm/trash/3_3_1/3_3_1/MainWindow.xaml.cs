c3ad9b9fdf25eb1f6b0fa5a5508c873f83d9a602
CheessieStew <serindrack@gmail.com>
2016-06-09 01:26:42 +0200

    Kitty freezes
    ;-;

---
+++ nvm/trash/3_3_1/3_3_1/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3_3_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public static string[] ComboBoxLabels = {"2-letnie", "3-letnie", "whatever"};
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (var label in ComboBoxLabels)
+            {
+                cyklCombo.Items.Add(label);
+            }
+            cyklCombo.SelectedIndex = 0;
+        }
+
+        private void AkceptujB_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(nazwaTB.Text);
+            sb.AppendLine();
+            sb.Append(adresTB.Text);
+            sb.AppendLine();
+            sb.Append(cyklCombo.Text);
+            sb.AppendLine();
+            if (dzienneCB.IsChecked != null && (bool)dzienneCB.IsChecked
+                && uzupełniająceCB.IsChecked != null && (bool)uzupełniająceCB.IsChecked)
+            {
+                sb.Append("Dzienne, uzupełniające");
+            }
+            else if (dzienneCB.IsChecked != null && (bool)dzienneCB.IsChecked)
+            {
+                sb.Append("Dzienne");
+            }
+            else if (uzupełniająceCB.IsChecked != null && (bool)uzupełniająceCB.IsChecked)
+            {
+                sb.Append("Uzupełniające");
+            }
+            MessageBox.Show(sb.ToString());
+        }
+
+        private void AnulujB_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ nvm/trash/3_3_2/3_3_2/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection;
+using System.Timers;
+
+namespace _3_3_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int rise;
+        ProgressBar[] bars;
+        Timer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            rise = 1;
+            slider.Minimum = 1;
+            slider.Maximum = 100;
+            slider.Value = 10;
+            bars = new ProgressBar[4] { PBar0, PBar1, PBar2, PBar3 };
+
+            foreach (var bar in bars)
+            {
+                bar.Minimum = 0;
+                bar.Maximum = 50;
+                bar.Value = 0;
+            }
+            timer = new Timer();
+            timer.Interval = 10;
+            timer.Elapsed += new ElapsedEventHandler(TickTock);
+            timer.Start();
+            mediaElement.Play();
+        }
+
+
+
+        void TickTock(object sender, EventArgs e)
+        {
+            this.Dispatcher.Invoke((Action)(() =>
+            {
+                try
+                {
+                    mediaElement.Play();
+                    if (PBar1.Value == PBar1.Maximum)
+                        rise = -1;
+                    if (PBar1.Value == PBar1.Minimum)
+                        rise = 1;
+                    foreach (var bar in bars)
+                        bar.Value += rise;
+                }
+                catch(TaskCanceledException ex)
+                { }
+            }));
+        }
+
+        private void mediaElement_MediaEnded(object sender, EventArgs e)
+        {
+            mediaElement.Stop();
+            mediaElement.Play();
+        }
+
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (timer!= null)
+              timer.Interval = slider.Value;
+        }
+    }
+}
